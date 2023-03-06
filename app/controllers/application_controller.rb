e3c75987438818308fa0c706143e74a7b2880dd8
Knoxx04 <onyiknoxx3@gmail.com>
2023-03-06 20:54:36 +0300

    created project routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,19 @@ class ApplicationController < Sinatra::Base
     projects = Project.all.to_json
   end
 
+  get '/projects/:id' do
+  id = params[:id]
+  project = Project.find_by(id: id)
+
+  if project
+    return project.to_json
+  else
+    status 404
+    return { message: "Project with ID #{id} not found." }.to_json
+  end
+end
+
+
   post '/projects' do 
     name = params[:name]
     details = params[:details]
@@ -17,5 +30,31 @@ class ApplicationController < Sinatra::Base
 
     projects = Project.create(name: name, details: details, user_id: user_id).to_json
   end
+
+
+  delete '/projects/:id' do
+  id = params[:id]
+  project = Project.find_by(id: id)
+
+  if project
+    project.destroy
+    return { message: "Project with ID #{id} has been deleted." }.to_json
+  else
+    status 404
+    return { message: "Project with ID #{id} not found." }.to_json
+  end
+end
+
+
+
+  
+
+  put '/projects/:id' do
+    projects = Project.find(params[:id])
+    projects.update(name: params[:name], details: params[:details])
+  end
+  
+
+
 
 end
